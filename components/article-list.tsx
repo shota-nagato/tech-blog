37437f48607bc0415bc671a506a54a62ba2d1c52
shota-nagato <shota.nagato@gmail.com>
2025-01-14 22:50:05 +0900

    記事を動的に取得

--- components/article-list.tsx
+++ components/article-list.tsx
@@ -1,28 +1,11 @@
 import Link from "next/link";
 import { Card, CardContent, CardFooter } from "./ui/card";
 import { Badge } from "./ui/badge";
+import { getArticles } from "@/lib/client";
 
-export default function ArticleList() {
-  const articles = [
-    {
-      id: 1,
-      title: "Article 1",
-      category: "Frontend",
-      date: "2024-12-12",
-    },
-    {
-      id: 2,
-      title: "Article 2",
-      category: "Backend",
-      date: "2024-12-12",
-    },
-    {
-      id: 3,
-      title: "Article 3",
-      category: "Data Science",
-      date: "2024-12-12",
-    },
-  ];
+export default async function ArticleList() {
+  const { contents: articles } = await getArticles();
+
   return (
     <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
       {articles.map((article) => (
@@ -30,11 +13,11 @@ export default function ArticleList() {
           <Card className="overflow-hidden">
             <div className="relative h-48 border">Image</div>
             <CardContent className="p-4">
-              <Badge>{article.category}</Badge>
+              <Badge>{article.category.name}</Badge>
               <h2 className="text-xl font-semibold">{article.title}</h2>
             </CardContent>
             <CardFooter className="text-sm text-slate-600">
-              {article.date}
+              {article.publishedAt}
             </CardFooter>
           </Card>
         </Link>
--- lib/client.ts
+++ lib/client.ts
@@ -1,4 +1,9 @@
-import { createClient } from "microcms-js-sdk";
+import {
+  createClient,
+  MicroCMSDate,
+  MicroCMSImage,
+  MicroCMSQueries,
+} from "microcms-js-sdk";
 
 if (!process.env.MICROCMS_SERVICE_DOMAIN) {
   throw new Error("MICROCMS_SERVICED_DOMAIN is required");
@@ -13,6 +18,14 @@ export type Category = {
   name: string;
 };
 
+export type Article = {
+  id: string;
+  title: string;
+  content: string;
+  eyecatch?: MicroCMSImage;
+  category: Category;
+} & MicroCMSDate;
+
 const client = createClient({
   serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
   apiKey: process.env.MICROCMS_API_KEY,
@@ -24,4 +37,13 @@ export async function getCategories() {
   });
 
   return categories;
+}
+
+export async function getArticles(queries?: MicroCMSQueries) {
+  const articles = await client.getList<Article>({
+    endpoint: "articles",
+    queries,
+  });
+
+  return articles;
 }

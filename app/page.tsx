34ea2e936e1bbab867edd6dc8a356806b5f2675d
shota-nagato <shota.nagato@gmail.com>
2025-03-09 15:42:46 +0900

    TOPページをSSRにする

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Pagination from "@/components/pagination";
 import { getArticles } from "@/lib/client";
 import { LIMIT } from "@/lib/constants";
 
+export const revalidate = 0;
+
 export default async function Home() {
   const { contents: articles, totalCount } = await getArticles({
     limit: LIMIT,
